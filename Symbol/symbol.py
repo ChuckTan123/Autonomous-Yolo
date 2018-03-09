0a85e1d6ff968aa597d021b5a3b391b96b28467b
Xuan TAN <xuan.tan@workday.com>
2018-03-09 14:10:17 -0800

    new yolov2

--- Symbol/symbol.py
+++ Symbol/symbol.py
@@ -1,4 +1,46 @@
+import unittest
+
 import mxnet as mx
+import numpy as np
+
+
+@mx.init.register
+class CustomConstant(mx.init.Initializer):
+    def __init__(self, value):
+        super(CustomConstant, self).__init__(value=value)
+        self.value = value
+
+    def _init_weight(self, _, arr):
+        arr[:] = mx.nd.array(self.value)
+
+
+class ConstantInitTest(unittest.TestCase):
+    def testInitCPU(self, ):
+        batch_size = 10
+
+        const_arr = (np.ones((5, 5)) * 10).tolist()
+        a = mx.sym.Variable('a', shape=(5, 5), init=CustomConstant(value=const_arr))
+        a = mx.sym.BlockGrad(a)  # now variable a is a constant
+
+        data = mx.sym.Variable('data')
+        loss = mx.sym.MakeLoss(mx.sym.broadcast_add(a * (-1), data) ** 2)
+
+        out = mx.sym.Group([loss, mx.sym.BlockGrad(a)])
+        mod = mx.mod.Module(out, data_names=['data'], label_names=[])
+        mod.bind(data_shapes=[('data', (batch_size, 5, 5)), ])
+        mod.init_params(initializer=mx.init.Uniform())
+        mod.init_optimizer(optimizer='sgd', optimizer_params=(('learning_rate', 0.005),))
+
+        # a = mx.nd.ones((5, 5))
+        data = np.ones((1000, 5, 5))
+
+        dataiter = mx.io.NDArrayIter(data={'data': data}, batch_size=batch_size)
+        dataiter.reset()
+        for batch_id, databatch in enumerate(dataiter):
+            mod.forward_backward(databatch)
+            mod.update()
+            assert (mod.get_outputs()[1].asnumpy() == const_arr).all()
+
 
 def expit_tensor(x):
     return 1/(1+mx.sym.exp(-x))
@@ -42,129 +84,72 @@ def YOLO_loss(predict, label):
     return loss
 
 
-def fcrn_loss_YoloV2(label, pred, anchors, config):
-    """Calculate loss specifically for FCRN
-    Need MXNET 0.11 or high34 !!!
-    Parameters
-    ----------
-    pred : mx.Symbol
-        predicted label (shape is (batch_size, 1024, 7*N_anchors))
-    label : mx.Symbol
-        ground truth label (shape is (batch_size, 7168))
-    Returns
-    -------
-    float
-    """
+def get_symbol_train_YoloV2_2(model_path, epoch):
 
-    # todo: test the function step by step
-    # meta data
-    sprob = 1
-    snoob = 0.5
-    scoor = 5
-    size_H, size_W, _ = (32,32)  # here we define the size is 32 by 32
-    B = 5
-    HW = size_H * size_W  # number of grid cells
+    # setup label
+    label = mx.sym.Variable("softmax")
 
-    # Read anchors, they should be 0<w,h<1
+    # setup the anchors
+    num_anchor = 5
+    anchors_num = [[0.10235532, 0.04075217],
+                    [0.29644471, 0.05406184],
+                    [0.04517153, 0.02728633],
+                    [0.1752567, 0.0486942],
+                    [0.55452996, 0.06665693]]
+    anchors = mx.sym.Variable("anchors", shape=(5, 2), dtype=np.float32,
+                              init=CustomConstant(anchors_num))
+    anchors = mx.sym.BlockGrad(anchors)
     anchors_w, anchors_h = mx.sym.split(anchors, axis=1, num_outputs=2, name="anchor_split")
 
-    # Read label
-    label_reshape = mx.sym.reshape(label, [-1, size_H, size_W, 9])
-    # |x_l|<0.5, |y_l|<0.5, 0<w_l & h_l<1, 0<cos_l<1, |sin_l|<1
-    prob_l, x_l, y_l, w_l, h_l, cls1, cls2, cls3, cls4 = mx.sym.split(label_reshape, axis=3, num_outputs=9, name="label_split")
-    # To compute the upperleft point xy and bottomright point xy, it is based on 32(for example), not the real WH
+    # read resnet
+    resnet, args, aux = mx.model.load_checkpoint(model_path, epoch)
+    resnet_interals = resnet.get_internals()['bn1_output']
 
-    upperleft_x_l = x_l - w_l * size_W * 0.5
-    upperleft_y_l = y_l - h_l * size_H * 0.5
-    bottomright_x_l = x_l + w_l * size_W * 0.5
-    bottomright_y_l = y_l + h_l * size_H * 0.5
-    upperleft_x_l =  -w_l * size_W * 0.5
-    upperleft_y_l = -h_l * size_H * 0.5
-    bottomright_x_l = w_l * size_W * 0.5
-    bottomright_y_l = h_l * size_H * 0.5
-    area_l = (w_l * h_l) * (size_W * size_H)
+    # objectness + classification
+    conv_obj = mx.sym.Convolution(data=resnet_interals, kernel=(3, 3), pad=(1, 1),
+                                  num_filter=5, name="obj_cls")
+    conv_coord = mx.sym.Convolution(data=resnet_interals, kernel=(3,3), pad=(1, 1),
+                                    num_filter=5*num_anchor, name="coord_anchor")
 
-    # Extrac the coordinate prediction from pred
-    pred_reshape = mx.sym.reshape(pred, [-1, size_H, size_W, B, 7])
-    prob_p, x_p, y_p, wr_p, hr_p, cls1, cls2, cls3, cls4 = mx.sym.split(pred_reshape, axis=4, num_outputs=9, name="pred_split")
-    x_adjust = expit_tensor(x_p) - 0.5  # sigmoid and shift it to -0.5~0.5
-    y_adjust = expit_tensor(y_p) - 0.5  # sigmoid and shift it to -0.5~0.5
-    w_adjust = mx.sym.sqrt(
-        mx.sym.broadcast_mul(mx.sym.exp(wr_p), mx.sym.reshape(anchors_w, shape=[1, 1, B, 1])))
-    h_adjust = mx.sym.sqrt(
-         mx.sym.broadcast_mul(mx.sym.exp(hr_p), mx.sym.reshape(anchors_h, shape=[1, 1, B, 1])))
-    prob_adjust = expit_tensor(prob_p)
+    # build up loss layer
+    # object + classification
+    cp, cls1, cls2, cls3, cls4 = mx.sym.split(conv_obj, num_outputs=5, axis=3)
+    prob_adjust = expit_tensor(cp)
     cls1p = expit_tensor(cls1)
     cls2p = expit_tensor(cls2)
     cls3p = expit_tensor(cls3)
     cls4p = expit_tensor(cls4)
 
-    # Get predict upperleft and bottomright
-    w_p = w_adjust ** 2
-    h_p = h_adjust ** 2
-    upperleft_x_p = x_p - w_p * size_W * 0.5  # will be negative, but it doesnt affect anything for computing IOU
-    upperleft_y_p = y_p - h_p * size_H * 0.5  # will be negative, but it doesnt affect anything for computing IOU
-    bottomright_x_p = x_p + w_p * size_W * 0.5  # will be negative, but it doesnt affect anything for computing IOU
-    bottomright_y_p = y_p + h_p * size_H * 0.5  # will be negative, but it doesnt affect anything for computing IOU
-    area_pred = (w_p * h_p) * (size_W * size_H)
 
-    # caculate intersection areas
-    intersect_upleft_x = mx.sym.broadcast_maximum(upperleft_x_p, mx.sym.expand_dims(upperleft_x_l, axis=-1))
-    intersect_upleft_y = mx.sym.broadcast_maximum(upperleft_y_p, mx.sym.expand_dims(upperleft_y_l, axis=-1))
-    intersect_botright_x = mx.sym.broadcast_minimum(bottomright_x_p, mx.sym.expand_dims(bottomright_x_l, axis=-1))
-    intersect_botright_y = mx.sym.broadcast_minimum(bottomright_y_p, mx.sym.expand_dims(bottomright_y_l, axis=-1))
-    intersect_w = mx.sym.maximum(intersect_botright_x - intersect_upleft_x, 0)
-    intersect_h = mx.sym.maximum(intersect_botright_y - intersect_upleft_y, 0)
-    intersect = intersect_w * intersect_h
+    # bbox regression
+    b1, x1, y1, w1, h1, \
+    b2, x2, y2, w2, h2, \
+    b3, x3, y3, w3, h3, \
+    b4, x4, y4, w4, h4, \
+    b5, x5, y5, w5, h5, = mx.sym.split(conv_coord, num_outputs=25, axis=3)
 
-    # calculate the best IOU, set 0.0 confidence for worse boxes
-    iou = intersect / mx.sym.broadcast_add(area_pred - intersect, mx.sym.expand_dims(area_l, axis=3))
-    best_box = mx.sym.broadcast_equal(iou, mx.sym.max_axis(iou, axis=3, keepdims=True)) * 1.0
-    prob_anchor_l = mx.sym.broadcast_mul(best_box, mx.sym.expand_dims(prob_l, axis=-1))
-    x_anchor_l = mx.sym.broadcast_mul(best_box, mx.sym.expand_dims(x_l, axis=-1), name="x_anchor_l")
-    y_anchor_l = mx.sym.broadcast_mul(best_box, mx.sym.expand_dims(y_l, axis=-1), name="y_anchor_l")
-    w_anchor_l = mx.sym.broadcast_mul(best_box, mx.sym.expand_dims(w_l, axis=-1), name="w_anchor_l")
-    h_anchor_l = mx.sym.broadcast_mul(best_box, mx.sym.expand_dims(h_l, axis=-1), name="h_anchor_l")
+    b_all = mx.sym.stack(b1, b2, b3, b4, b5, axis=3)
+    x_all = mx.sym.stack(x1, x2, x3, x4, x5, axis=3)
+    y_all = mx.sym.stack(y1, y2, y3, y4, y5, axis=3)
+    w_all = mx.sym.stack(w1, w2, w3, w4, w5, axis=3)
+    h_all = mx.sym.stack(h1, h2, h3, h4, h5, axis=3)
 
-    # set up weighted sum
-    mask = (prob_anchor_l * 5 + (1 - prob_anchor_l) * .5)  # balance box and non-boxes
-
-    loss_prob = mx.sym.LinearRegressionOutput(data=prob_adjust * mask, label=prob_anchor_l * mask, grad_scale=1,
-                                              name="lossprob")
-    loss_x = mx.sym.LinearRegressionOutput(data=mx.sym.broadcast_mul(x_adjust, best_box), label=x_anchor_l,
-                                         grad_scale=scoor, name="lossx")
-    loss_y = mx.sym.LinearRegressionOutput(data=y_adjust, label=y_anchor_l, grad_scale=scoor, name="lossy")
-    loss_w = mx.sym.LinearRegressionOutput(data=w_adjust, label=w_anchor_l, grad_scale=scoor, name="lossw")
-    loss_h = mx.sym.LinearRegressionOutput(data=h_adjust, label=h_anchor_l, grad_scale=scoor, name="lossh")
-    loss_cls1 = mx.sym.LinearRegressionOutput(data=cls1p, label=cls1, grad_scale=scoor, name="losscos")
-    loss_cls2 = mx.sym.LinearRegressionOutput(data=cls2p, label=cls2, grad_scale=scoor, name="losssin")
-    loss_cls3 = mx.sym.LinearRegressionOutput(data=cls3p, label=cls3, grad_scale=scoor, name="losssin")
-    loss_cls4 = mx.sym.LinearRegressionOutput(data=cls4p, label=cls4, grad_scale=scoor, name="losssin")
-    loss = loss_prob + loss_x + loss_y + loss_w + loss_h + loss_cls1 + loss_cls2 + loss_cls3 + loss_cls4
-    return loss
+    bprob = expit_tensor(b_all)
+    x_adjust = expit_tensor(x_all) - 0.5  # sigmoid and shift it to -0.5~0.5
+    y_adjust = expit_tensor(y_all) - 0.5  # sigmoid and shift it to -0.5~0.5
+    w_adjust = mx.sym.sqrt(
+        mx.sym.broadcast_mul(mx.sym.exp(w_all), mx.sym.reshape(anchors_w, shape=[1, 1, num_anchor, 1])))
+    h_adjust = mx.sym.sqrt(
+        mx.sym.broadcast_mul(mx.sym.exp(h_all), mx.sym.reshape(anchors_h, shape=[1, 1, num_anchor, 1])))
 
 
-# Get pretrained imagenet model
-def get_resnet_model(model_path, epoch):
-    # not necessary to be this name, you can do better
-    label = mx.sym.Variable('softmax_label')
-    # load symbol and actual weights
-    sym, args, aux = mx.model.load_checkpoint(model_path, epoch)
-    # extract last bn layer
-    sym = sym.get_internals()['bn1_output']
-    # append two layers
-    sym = mx.sym.Activation(data=sym, act_type="relu", name="relu_final")
-    sym = mx.sym.Convolution(data=sym, kernel=(3, 3),
-                             num_filter=9, pad=(1, 1),
-                             stride=(1, 1), no_bias=True,
-                             )
-    # get softsign
-    sym = sym / (1 + mx.sym.abs(sym))
-    logit = mx.sym.transpose(sym, axes=(0, 2, 3, 1), name="logit") # (-1, 7, 7, 5(c,x,y,w,h))
-    # apply loss
-    loss_ = YOLO_loss(logit, label)
-    # mxnet special requirement
-    loss = mx.sym.MakeLoss(loss_)
-    # multi-output logit should be blocked from generating gradients
-    out = mx.sym.Group([loss, mx.sym.BlockGrad(logit)])
-    return out
+    # todo: either do translation on GT, or do the mapping here.
+    # prob_adjust cls1p cls2p cls3p cls4p
+    # bprob, x_adjust, y_adjust, w_adujst, h_adjust
+
+
+
+
+
+
+
